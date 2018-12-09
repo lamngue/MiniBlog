3495ff6ecc807f1a165d1bf6cb9cf4adb8f87e70
Lam Nguyen <lnguye16@gustavus.edu>
2018-12-08 21:12:58 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PostsIndex from "./components/post_index.js";
+import PostNew from "./components/post_new.js";
+import PostShow from "./components/post_show.js";
+import RegisterUser from "./components/registerUser.js";
+import RenderUser from "./components/renderUser.js";
+import LoginUser from "./components/loginUser.js";
+import promise from "redux-promise";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+			<div>
+				<Switch>
+					<Route path="/login" component={LoginUser} />
+					<Route path="/posts/new" component={PostNew} />
+					<Route path="/posts/:id" component={PostShow} />
+					<Route path="/posts" component={PostsIndex} />
+					<Route path="/user/:id" component={RenderUser} />
+					<Route path="/" component={RegisterUser} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>,
+	document.querySelector("#root")
+);
