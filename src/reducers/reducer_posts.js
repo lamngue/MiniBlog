b50f11698ff4b27cbb24689f43f1384603fe4010
Lam Nguyen <lnguye16@gustavus.edu>
2018-12-22 22:48:53 -0600

    added like feature

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -18,14 +18,13 @@ export default function(state = {}, action) {
 			return _.mapKeys(action.payload.data, "_id");
 		case FETCH_POST:
 			const post = action.payload.data;
-			///console.log(post);
+			//console.log(post);
 			newState = { ...state };
 			newState[post._id] = post;
 			return newState;
 		case LIKE_POST:
 			replacePost = action.payload.data;
 			allPosts = _.values({...state});
-			console.log(allPosts);
 			index = _.findIndex(allPosts, {_id: replacePost._id});
 			// Replace item at index using native splice
 			allPosts.splice(index, 1, replacePost);
@@ -33,7 +32,6 @@ export default function(state = {}, action) {
 		case UNLIKE_POST:
 			replacePost = action.payload.data;
 			allPosts = _.values({...state});
-			console.log(allPosts);
 			index = _.findIndex(allPosts, {_id: replacePost._id});
 			// Replace item at index using native splice
 			allPosts.splice(index, 1, replacePost);
